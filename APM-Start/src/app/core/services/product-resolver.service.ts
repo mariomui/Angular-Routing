ecd24b19a827d8f62dc7c7f54747165dccd19149
mariomui <yi.kan.mui@gmail.com>
2020-04-29 17:39:58 -0700

    Populate product resolver service with explanations and initial build

--- APM-Start/src/app/core/services/product-resolver.service.ts
+++ APM-Start/src/app/core/services/product-resolver.service.ts
@@ -1,9 +1,43 @@
-import { Injectable } from '@angular/core';
+import { Injectable, ɵCompiler_compileModuleSync__POST_R3__ } from '@angular/core';
+import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Product } from 'src/app/products/product';
+import { Observable } from 'rxjs';
+import { ProductService } from 'src/app/products/product.service';
+
 
 @Injectable({
   providedIn: 'root'
 })
-export class ProductResolverService {
+// Providing a singleton service
+// There are two ways to make a service a singleton in Angular:
 
-  constructor() { }
+// Set the providedIn property of the @Injectable() to "root".
+// Include the service in the AppModule or in a module that is only imported by the AppModule
+export class ProductResolverService implements Resolve<Product> {
+  // Resolve is an typescript interface
+  // https://angular.io/api/router/Resolve
+  // interface Resolve<T> {
+  //   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<T> | Promise<T> | T
+  // }
+  constructor(private productService: ProductService) {
+
+  }
+
+
+  // these two snapshots imported from router arent objects to be sued, they are interfaces and only describes the route and the state.
+  /**
+   * 
+   * @param route Activated Route snapshot is the current activatedRoute
+   * @param state // TODO Explain what a Router StateSnapshot is.
+   */
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Product> {
+    const id = + route.paramMap.get('id');
+    // resolve must called during some kind of routing process.
+    if (isNaN(+id)) {
+      const message = `Product id was not a number: ${id}`;
+      console.error(message);
+      return;
+    }
+    return null;
+  }
 }
